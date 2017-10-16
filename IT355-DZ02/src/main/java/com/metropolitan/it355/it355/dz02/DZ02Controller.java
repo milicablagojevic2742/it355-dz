1ab46c4a31bf023547fd82ce448a65bd740d11ac
milicablagojevic2742 <milica.blagojevic.2742@metropolitan.edu.rs>
2017-10-16 19:19:04 +0200

    IT355-DZ02

---
+++ IT355-DZ02/src/main/java/com/metropolitan/it355/it355/dz02/DZ02Controller.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metropolitan.it355.it355.dz02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Blagojevic Milica
+ */
+
+@Controller
+@RequestMapping("/")
+public class DZ02Controller {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Dobrodošli u online prodavnicu auto delova!");
+        return "DZ02";
+
+    }
+}
